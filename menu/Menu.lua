da16399736cd1bc564b00a2da4237f5391297c76
Harybo <worplextoto@gmail.com>
2020-08-24 16:43:21 +0200

    up

--- menu/Menu.lua
+++ menu/Menu.lua
@@ -44,8 +44,8 @@ function RageUI.CreateMenu(Title, Subtitle, X, Y, TextureDictionary, TextureName
     Menu.Open = false
     Menu.Controls = RageUI.Settings.Controls
     Menu.Index = 1
-    Menu.Sprite = (R == nil and { Dictionary = TextureDictionary or "commonmenu", Texture = TextureName or "interaction_bgd" }) or nil
-    Menu.Rectangle = (TextureDictionary == nil and { R = tonumber(R) or 255, G = tonumber(G) or 255, B = tonumber(B) or 255, A = tonumber(A) or 255 }) or nil
+    Menu.Sprite = TextureDictionary and { Dictionary = TextureDictionary or "commonmenu", Texture = TextureName or "interaction_bgd" } or nil
+    Menu.Rectangle = R and { R = tonumber(R) or 255, G = tonumber(G) or 255, B = tonumber(B) or 255, A = tonumber(A) or 255 } or nil
     Menu.Pagination = { Minimum = 1, Maximum = 10, Total = 10 }
     Menu.Safezone = true
     Menu.SafeZoneSize = nil
